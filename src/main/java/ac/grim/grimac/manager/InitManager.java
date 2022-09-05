cc7d683e6b8e9a0b0b3c717c92a8d2acdddbcc50
jquery-package <78819641+jquery-package@users.noreply.github.com>
2022-09-06 02:49:32 +0300

    Update InitManager.java

--- src/main/java/ac/grim/grimac/manager/InitManager.java
+++ src/main/java/ac/grim/grimac/manager/InitManager.java
@@ -27,7 +27,6 @@ public class InitManager {
                 .put(TickRunner.class, new TickRunner())
                 .put(TickEndEvent.class, new TickEndEvent())
                 .put(CommandRegister.class, new CommandRegister())
-                .put(BStats.class, new BStats())
                 .put(PacketLimiter.class, new PacketLimiter())
                 .put(DiscordManager.class, GrimAPI.INSTANCE.getDiscordManager())
                 .put(SpectateManager.class, GrimAPI.INSTANCE.getSpectateManager())
