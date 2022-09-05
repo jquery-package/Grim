ecbbde8802b3e5558eed75a9fd6352edad667b6f
jquery-package <78819641+jquery-package@users.noreply.github.com>
2022-09-06 02:26:09 +0300

    Create MultiLib.java

---
+++ com/github/puregero/multilib/MultiLib.java
@@ -0,0 +1,11 @@
+package com.github.puregero.multilib;
+
+import org.bukkit.entity.Player;
+
+public class MultiLib {
+ 
+    public boolean isExternalPlayer(Player player) {
+         return false; 
+    }
+  
+}
